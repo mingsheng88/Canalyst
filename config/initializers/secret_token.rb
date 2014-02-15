3bbceff1131d3deba5eda37ac6fc4d7d944d92ee
mingsheng88 <mingsheng88@hotmail.com>
2014-02-15 15:57:06 +0800

    Empty project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Canalyst::Application.config.secret_key_base = '909eae5352c92f4d530cc8f870bdadc09f2d7a94289f7a537559c3f73cd6147d23a03924259fc16233e10b870a5a3c34c9b94368ab0906ff1e8db797fea225e1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Canalyst::Application.config.session_store :cookie_store, key: '_canalyst_session'
